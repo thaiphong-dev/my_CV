8f2be2d2f8481d9d7f7060577897c1de009c727d
thaiphong1101 <tuthaiphong600@gmail.com>
2020-09-19 10:58:48 +0700

    js

---
+++ resourses/javascript/main.js
@@ -0,0 +1,7 @@
+var boxHTMLCollection = document.getElementsByClassName('box');
+// var box1Element = document.querySelector('.box');
+var box1Element = boxHTMLCollection[0];
+var box2Element = boxHTMLCollection[1];
+var allItemElements = document.getElementsByTagName('li');
+var box1ItemElements = document.box1Element.getElementsByTagName('li');
+var box2ItemElements = document.box2Element.getElementsByTagName('li');
